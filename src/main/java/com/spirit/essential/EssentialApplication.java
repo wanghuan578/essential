6353b562820c17d4e32cdf417df84b25e2e80001
王欢 <wanghuan@beadwallet.com>
2019-08-08 12:00:58 +0800

    create

---
+++ src/main/java/com/spirit/essential/EssentialApplication.java
@@ -0,0 +1,13 @@
+package com.spirit.essential;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EssentialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EssentialApplication.class, args);
+    }
+
+}
